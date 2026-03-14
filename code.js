aa683153a18543c8e7d48630c60700e73b947435
Dnyanu-Ingale <dnyaneshwaringale0905@gmail.com>
2026-03-14 14:05:55 +0530

    Modified code

--- code.js
+++ code.js
@@ -1,18 +1,21 @@
-let n = 4;
-let size = 2 * n - 1;
+function printPattern(n) {
+  let size = 2 * n - 1;
 
-for (let i = 0; i < size; i++) {
-  let row = "";
+  for (let i = 0; i < size; i++) {
+    let row = "";
 
-  for (let j = 0; j < size; j++) {
-    let top = i;
-    let left = j;
-    let bottom = size - 1 - i;
-    let right = size - 1 - j;
+    for (let j = 0; j < size; j++) {
+      let top = i;
+      let left = j;
+      let bottom = size - 1 - i;
+      let right = size - 1 - j;
 
-    let minDistance = Math.min(top, left, bottom, right);
-    row += (n - minDistance) + " ";
+      let minDistance = Math.min(top, left, bottom, right);
+      row += (n - minDistance) + " ";
+    }
+
+    console.log(row.trim());
   }
-
-  console.log(row.trim());
 }
+
+printPattern(4);
